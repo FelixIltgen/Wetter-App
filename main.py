1767bc77ca2bdd3ceaf9cde2e473dae0a17890fa
Felix Iltgen <felixiltgen@hotmail.de>
2025-02-24 12:15:12 +0100

    Extracted necessary data

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 from weather_api import *
 
 class Main:
-    
+    weather_data = {}
     user_input = ""
     def __init__(self):
         pass
     
     def main() -> None:
         Main.start_weather_app()
+        Main.extrtact_weather_data(Main.weather_data)
+        Main.build_gui(Main.weather_data)
     
     def start_weather_app() -> None:
         print("Wilkommen bei der Wetter-App")
         print("****************************")
         Main.ask_for_user_input()
     
+    def extrtact_weather_data(data=dict) -> dict:
+        data_list = [new_data for new_data in data if new_data == "weather" or new_data =="main"]
+        dict_data = {k: v for (k,v) in data[data_list[0]][0].items()}
+        Main.weather_data = dict_data.update(data[data_list[1]])
+    
     def build_gui() -> None:
         pass
-    
-    def get_weather_icon() -> None:
-        pass
-    
+          
     def ask_for_user_input():
         Main.user_input = input("Gebe ein Standort an: ")
-        print(Weather_api.convert_name_in_location(Main.user_input))
+        Main.weather_data = Weather_api.convert_name_in_location(Main.user_input)
 
 if __name__ == "__main__":
     Main.main()
--- weather_api.py
+++ weather_api.py
@@ -8,26 +8,24 @@ class Weather_api:
     def __init__(self):
         pass
     
-    def request_current_weather_data():
+    def request_current_weather_data() -> dict:
         url = f"{Weather_api.basic_url}/data/2.5/weather?lat={Weather_api.lat}&lon={Weather_api.lon}&appid={Weather_api.api_key}&units=metric&lang=de"
         response = requests.get(url)
         response_data = response.json()
-        Weather_api.extract_weather_data(response_data)
+        print(response_data)
+        return response_data
     
     def request_forecast() -> dict:
         pass
-    
-    def extract_weather_data(data) -> dict:
-        pass
-    
-    def convert_name_in_location(user_input) -> None:
+     
+    def convert_name_in_location(user_input):
         url = f"{Weather_api.basic_url}/geo/1.0/direct?q={user_input}&limit=1&appid={Weather_api.api_key}"
         response = requests.get(url)
         if(response.status_code == 200):
             response_data = response.json()
-            response_dict = response_data[0]
+            response_dict = response_data[0] #==> Wirft error wenn Stanndort nicht exsisitiert
             Weather_api.lat = str(response_dict["lat"])
             Weather_api.lon = str(response_dict["lon"])
-            Weather_api.request_current_weather_data()
+            return Weather_api.request_current_weather_data()
         else: 
             print(f"Daten konnten nicht gefunden werden! {response.status_code}")
