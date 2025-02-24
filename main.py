891e042daad42a85c546edd2d4e860b06d67e3bb
Felix Iltgen <felixiltgen@hotmail.de>
2025-02-24 13:20:25 +0100

    Added Console output

--- main.py
+++ main.py
@@ -19,10 +19,19 @@ class Main:
     def extrtact_weather_data(data=dict) -> dict:
         data_list = [new_data for new_data in data if new_data == "weather" or new_data =="main"]
         dict_data = {k: v for (k,v) in data[data_list[0]][0].items()}
-        Main.weather_data = dict_data.update(data[data_list[1]])
+        dict_data.update(data[data_list[1]])
+        Main.weather_data = dict_data
+        print(Main.weather_data)
     
-    def build_gui() -> None:
-        pass
+    def build_gui(weather) -> None:
+        print(f"Aktuelles Wetter für {Main.user_input}".center(48))
+        print(f"************************************************")
+        print(f"Wetter: {weather["description"]}")
+        print(f"Aktuelle Temperatur: {weather["temp"]:.1f} C° | Gefühlt: {weather["feels_like"]:.1f} C°")
+        print(f"Minimal: {weather["temp_min"]:.1f} C° und maximal {weather["temp_max"]:.1f} C°")
+        print(f"Luftfeuchtigkeit: {weather["humidity"]} %")
+        print(f"************************************************")
+        
           
     def ask_for_user_input():
         Main.user_input = input("Gebe ein Standort an: ")
