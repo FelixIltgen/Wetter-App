c5f206483c71064f4b8ace55ba9e92fdecbde049
Felix Iltgen <felixiltgen@hotmail.de>
2025-02-26 11:46:54 +0100

    added stylesheet file

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from weather_api import *
+from style_sheet import *
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QDesktopWidget, QLineEdit, QPushButton
 from PyQt5.QtGui import QIcon, QFont, QPixmap
@@ -10,21 +11,21 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Wetter App")
         self.setWindowIcon(QIcon("pictures//app_icon.png"))
         self.setGeometry(0,0,600,800)
-        self.setStyleSheet("background-color: #62c8f0")
-
+        
         qtRectangle = self.frameGeometry()
         centerPoint = QDesktopWidget().availableGeometry().center()
         qtRectangle.moveCenter(centerPoint)
         self.move(qtRectangle.topLeft())
 
-        self.headline = QLabel("Überschrift", self)
+        self.headline = QLabel("Überschrift",self)
         self.input_field = QLineEdit("Test", self)
-        self.button = QPushButton("Send", self)
+        self.button_search = QPushButton("Send", self)
         self.weather_pic = QLabel(self)
         #self.pixmap = QPixmap("")=> Kann glaube ichauch erst in funktion erstellt werden
         self.output_label = QLabel(self)
 
         self.init_ui()
+        self.setStyleSheet(Style_Sheet.css_content)
 
     def init_ui(self):
 
@@ -37,9 +38,10 @@ class MainWindow(QMainWindow):
                                            "text-decoration: underline;")
         self.weather_pic.setGeometry(0,0,300,300)
         self.weather_pic.setGeometry((self.width()-self.weather_pic.width())//2,(self.height()-self.weather_pic.height())//2-50,self.weather_pic.width(),self.weather_pic.height())
-        self.weather_pic.setStyleSheet("background-color: #0a0100;")
+        self.weather_pic.setStyleSheet("background-color: black;")
 
         self.input_field.setGeometry(200,130,200,50)
+        self.button_search.setGeometry(400,130,100,50)
         
     
         
---
+++ style_sheet.py
@@ -0,0 +1,13 @@
+
+class Style_Sheet:
+    
+    css_content = """
+    
+    QLineEdit{
+        
+        border: 2px solid black;
+        background-color: green;
+    }
+    
+    
+    """
